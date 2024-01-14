d15c155dbdf10a1f55564fde88fc9c9da79549f9
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-15 05:28:29 +0700

    Initial commit

---
+++ src/main/java/zuhaproject/restful/RestfulApiSpringApplication.java
@@ -0,0 +1,13 @@
+package zuhaproject.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiSpringApplication.class, args);
+	}
+
+}
