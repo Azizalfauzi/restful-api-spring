d5d72179db82aa31e1535559740d0559be5efbc5
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-18 13:23:22 +0700

    * RestFul Spring - creating user controller and handle about function register

---
+++ src/main/java/zuhaproject/restful/controller/UserController.java
@@ -0,0 +1,23 @@
+package zuhaproject.restful.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import zuhaproject.restful.model.RegisterUserRequest;
+import zuhaproject.restful.model.WebResponse;
+import zuhaproject.restful.service.UserService;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(path = "/api/users", consumes = MediaType.APPLICATION_JSON_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
+        userService.register(request);
+        return WebResponse.<String>builder().data("OK").build();
+    }
+}
