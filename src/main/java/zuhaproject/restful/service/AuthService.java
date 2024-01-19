5c5ce8a5c160ae9ef7b6addd16251f2e5d1e971b
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-19 13:01:43 +0700

    * RestFul Spring - finishing method login in AuthService.java

--- src/main/java/zuhaproject/restful/service/AuthService.java
+++ src/main/java/zuhaproject/restful/service/AuthService.java
@@ -1,10 +1,17 @@
 package zuhaproject.restful.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+import zuhaproject.restful.entity.User;
 import zuhaproject.restful.model.LoginUserRequest;
 import zuhaproject.restful.model.TokenResponse;
 import zuhaproject.restful.repository.UserRepository;
+import zuhaproject.restful.security.BCrypt;
+
+import java.util.UUID;
 
 @Service
 public class AuthService {
@@ -13,8 +20,30 @@ public class AuthService {
     @Autowired
     private ValidationService validationService;
 
-//    public TokenResponse login(LoginUserRequest request) {
-//        validationService.validate(request);
-//
-//    }
+    @Transactional
+    public TokenResponse login(LoginUserRequest request) {
+        validationService.validate(request);
+
+        User user = userRepository.findById(request.getUsername()).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username or Password wrong"));
+
+        if (BCrypt.checkpw(request.getPassword(), user.getPassword())) {
+//            sukses
+            user.setToken(UUID.randomUUID().toString());
+            user.setTokenExpiredAt(next30Day());
+            userRepository.save(user);
+
+            return TokenResponse.builder()
+                    .token(user.getToken())
+                    .expiredAt(user.getTokenExpiredAt())
+                    .build();
+        } else {
+//            gagal
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username or Password wrong");
+        }
+    }
+
+    private Long next30Day() {
+        return System.currentTimeMillis() + (1000 * 16 * 24 * 30);
+    }
 }
