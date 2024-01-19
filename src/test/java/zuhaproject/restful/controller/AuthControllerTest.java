a250a3273728529b320782d117f339a6c4dc4893
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-19 13:27:32 +0700

    * RestFul Spring - create unit test LoginSuccess and finish authentication API

--- src/test/java/zuhaproject/restful/controller/AuthControllerTest.java
+++ src/test/java/zuhaproject/restful/controller/AuthControllerTest.java
@@ -2,6 +2,7 @@ package zuhaproject.restful.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.val;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import zuhaproject.restful.entity.User;
 import zuhaproject.restful.model.LoginUserRequest;
+import zuhaproject.restful.model.TokenResponse;
 import zuhaproject.restful.model.WebResponse;
 import zuhaproject.restful.repository.UserRepository;
 import zuhaproject.restful.security.BCrypt;
@@ -75,6 +77,35 @@ class AuthControllerTest {
                     WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
                     });
                     Assertions.assertNotNull(response.getErrors());
+                });
+    }
+
+    @Test
+    void loginSuccess() throws Exception {
+        User user = new User();
+        user.setName("test");
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        userRepository.save(user);
+
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("test");
+        request.setPassword("test");
+        mockMvc.perform(post("/api/auth/login").
+                        accept(MediaType.APPLICATION_JSON).
+                        contentType(MediaType.APPLICATION_JSON).
+                        content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(status().isOk()).andDo(result -> {
+                    WebResponse<TokenResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+                    });
+                    Assertions.assertNull(response.getErrors());
+                    Assertions.assertNotNull(response.getData().getToken());
+                    Assertions.assertNotNull(response.getData().getExpiredAt());
+
+                    User user1 = userRepository.findById("test").orElse(null);
+                    Assertions.assertNotNull(user1);
+                    Assertions.assertEquals(user1.getToken(), response.getData().getToken());
+                    Assertions.assertEquals(user1.getTokenExpiredAt(), response.getData().getExpiredAt());
                 });
     }
 }
