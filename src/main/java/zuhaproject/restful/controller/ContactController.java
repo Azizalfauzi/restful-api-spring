1fcc3060e798e89a125e82b442c5d0117afba8f7
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-26 05:48:15 +0700

    * RestFul Spring - adding new method get in ContactController.java.java

--- src/main/java/zuhaproject/restful/controller/ContactController.java
+++ src/main/java/zuhaproject/restful/controller/ContactController.java
@@ -1,11 +1,10 @@
 package zuhaproject.restful.controller;
 
 
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import zuhaproject.restful.entity.User;
 import zuhaproject.restful.model.ContactResponse;
 import zuhaproject.restful.model.CreateContactRequest;
@@ -24,6 +23,12 @@ public class ContactController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public WebResponse<ContactResponse> create(User user, @RequestBody CreateContactRequest request) {
         ContactResponse contactResponse = contactService.create(user, request);
+        return WebResponse.<ContactResponse>builder().data(contactResponse).build();
+    }
+
+    @GetMapping(path = "/api/contacts/{contactId}")
+    public WebResponse<ContactResponse> get(User user, @PathVariable("contactId") String contactId) {
+        ContactResponse contactResponse = contactService.get(user, contactId);
         return WebResponse.<ContactResponse>builder().data(contactResponse).build();
     }
 }
