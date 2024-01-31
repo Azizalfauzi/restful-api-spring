07098fa2a828150304564a745a771f86e87d26d6
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-31 09:43:11 +0700

    * RestFul Spring - create service deleteContact

--- src/main/java/zuhaproject/restful/service/ContactService.java
+++ src/main/java/zuhaproject/restful/service/ContactService.java
@@ -71,4 +71,12 @@ public class ContactService {
 
         return toContactResponse(contact);
     }
+
+    @Transactional
+    public void delete(User user, String contactId) {
+        Contact contact = contactRepository.findFirstByUserAndId(user, contactId).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact is not found"));
+
+        contactRepository.delete(contact);
+    }
 }
