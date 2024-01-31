728fcc1094b0ff7033fc400255892b8e7561d11c
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-31 09:44:42 +0700

    * RestFul Spring - create controller deleteContact

--- src/main/java/zuhaproject/restful/controller/ContactController.java
+++ src/main/java/zuhaproject/restful/controller/ContactController.java
@@ -43,4 +43,10 @@ public class ContactController {
         ContactResponse contactResponse = contactService.update(user, request);
         return WebResponse.<ContactResponse>builder().data(contactResponse).build();
     }
+
+    @DeleteMapping(path = "/api/contacts/{contactId}")
+    public WebResponse<String> delete(User user, @PathVariable("contactId") String contactId) {
+        contactService.delete(user, contactId);
+        return WebResponse.<String>builder().data("OK").build();
+    }
 }
