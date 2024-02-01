3806955ff62a72794987e52d585b8a9f2b1e07c4
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-01 09:12:04 +0700

    * RestFul Spring - create unit test method searchContactByName by last name

--- src/test/java/zuhaproject/restful/controller/ContactControllerTest.java
+++ src/test/java/zuhaproject/restful/controller/ContactControllerTest.java
@@ -288,5 +288,21 @@ class ContactControllerTest {
                     assertEquals(0, response.getPaging().getCurrentPage());
                     assertEquals(10, response.getPaging().getSize());
                 });
+
+        mockMvc.perform(get("/api/contacts").
+                        queryParam("name", "alfa").
+                        accept(MediaType.APPLICATION_JSON).
+                        contentType(MediaType.APPLICATION_JSON).
+                        header("X-API-TOKEN", "test")).
+                andExpectAll(status().isOk()).
+                andDo(result -> {
+                    WebResponse<List<ContactResponse>> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+                    });
+                    assertNull(response.getErrors());
+                    assertEquals(10, response.getData().size());
+                    assertEquals(10, response.getPaging().getTotalPage());
+                    assertEquals(0, response.getPaging().getCurrentPage());
+                    assertEquals(10, response.getPaging().getSize());
+                });
     }
 }
