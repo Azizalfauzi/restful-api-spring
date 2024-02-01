d08d6f307c0bbdc8d257bc96cdeb3ace29513098
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-01 09:16:23 +0700

    * RestFul Spring - create unit test method searchSuccess by phone

--- src/test/java/zuhaproject/restful/controller/ContactControllerTest.java
+++ src/test/java/zuhaproject/restful/controller/ContactControllerTest.java
@@ -320,5 +320,21 @@ class ContactControllerTest {
                     assertEquals(0, response.getPaging().getCurrentPage());
                     assertEquals(10, response.getPaging().getSize());
                 });
+
+        mockMvc.perform(get("/api/contacts").
+                        queryParam("phone", "123").
+                        accept(MediaType.APPLICATION_JSON).
+                        contentType(MediaType.APPLICATION_JSON).
+                        header("X-API-TOKEN", "test")).
+                andExpectAll(status().isOk()).
+                andDo(result -> {
+                    WebResponse<List<ContactResponse>> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+                    });
+                    assertNull(response.getErrors());
+                    assertEquals(10, response.getData().size());
+                    assertEquals(10, response.getPaging().getTotalPage());
+                    assertEquals(0, response.getPaging().getCurrentPage());
+                    assertEquals(10, response.getPaging().getSize());
+                });
     }
 }
